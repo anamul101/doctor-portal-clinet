b885ad2b5e27f271a8a39aeb5ee2146dd80f10ff
anamul101 <rjanamul01871@gmail.com>
2022-11-15 08:15:52 +0000

    authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routers/Routers';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
